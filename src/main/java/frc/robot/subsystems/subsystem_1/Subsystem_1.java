804ef40061453e276037a1bb34e91bd528dc1154
Mateo Johnson <johnson.12@themetroschool.org>
2024-04-08 10:31:41 -0400

    added subsystem files

--- src/main/java/frc/robot/subsystems/subsystem_1/Subsystem_1.java
+++ src/main/java/frc/robot/subsystems/subsystem_1/Subsystem_1.java
@@ -1,5 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.subsystem_1;
 
-public class Subsystem_1 {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class subsystem_1 extends SubsystemBase {
+  /** Creates a new subsystem_3. */
+  public subsystem_1() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
--- src/main/java/frc/robot/subsystems/subsystem_2/subsystem_2.java
+++ src/main/java/frc/robot/subsystems/subsystem_2/subsystem_2.java
@@ -1,5 +1,17 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
 package frc.robot.subsystems.subsystem_2;
 
-public class subsystem_2 {
-    
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class subsystem_2 extends SubsystemBase {
+  /** Creates a new subsystem_2. */
+  public subsystem_2() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
 }
