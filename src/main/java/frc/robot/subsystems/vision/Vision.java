6f48686effc225ea0870cfa8adef0600d3941f2a
Mateo Johnson <johnson.12@themetroschool.org>
2024-07-10 19:14:24 -0400

    silly

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -6,121 +6,107 @@ import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.utils.LimelightLib;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Vision extends SubsystemBase {
 
-public static String limelight = "limelight";
+    private static final Logger LOGGER = Logger.getLogger(Vision.class.getName());
 
+    private static final String LIMELIGHT = "limelight";
 
-//OK SO GUYS IF SOMETHING IS MARKED WITH AN  THAT MEANS THAT ITS FROM THE ARM LIMELIGHT
+    // Coordinate value getters
+    private double tX;
+    private double tY;
+    private double tA;
+    private boolean tV;
 
-//COORDINATE VALUE GETTERS
-public static double tX = LimelightLib.getTX(limelight);
-public static double tY = LimelightLib.getTY(limelight);
-public static double tA = LimelightLib.getTA(limelight);
-public static boolean tV = LimelightLib.getTV(limelight);
+    // Robot pose getters
+    private Pose3d botPose3d;
+    private double[] botPoseTargetSpace;
+    private double[] botPose3dBlue;
+    private double[] botPose3dRed;
+    private double[] botPose;
+    private Pose2d botPose2d;
+    private Pose2d botPose2dBlue;
+    private Pose2d botPose2dRed;
 
-//ROBOT POSE GETTERS 
-//3D
-public static Pose3d botPose3d = LimelightLib.getBotPose3d(limelight);
-public static double[] botPoseTargetSpace = LimelightLib.getBotPose_TargetSpace(limelight);
-public static double[] botPose3dBlue = LimelightLib.getBotPose_wpiBlue(limelight);
-public static double[] botPose3dRed = LimelightLib.getBotPose_wpiRed(limelight);
+    // Camera pose getters
+    private Pose3d camPoseRobotSpace;
+    private Pose3d camPose3dRobotSpace;
+    private double[] camPose2dRobotSpace;
 
-//2D
-public static double[] botPose = LimelightLib.getBotPose(limelight);
-public static Pose2d botPose2d = LimelightLib.getBotPose2d(limelight);
-public static Pose2d botPose2dBlue = LimelightLib.getBotPose2d_wpiBlue(limelight);
-public static Pose2d botPose2dRed = LimelightLib.getBotPose2d_wpiRed(limelight);
+    // Target info getters
+    private Pose3d targetPose3dCamSpace;
+    private double targetID;
+    private double[] targetColor;
+    private Translation3d targetPosition;
 
-//CAMERA POSE GETTERS
-public static Pose3d camPoseRobotSpace = LimelightLib.getCameraPose3d_RobotSpace(limelight);
-public static Pose3d camPose3dRobotSpace = LimelightLib.getCameraPose3d_TargetSpace(limelight);
-public static double[] camPose2dRobotSpace = LimelightLib.getCameraPose_TargetSpace(limelight);
+    // Latency getters
+    private double pipelineIndex;
+    private double captureLatency;
+    private double pipelineLatency;
 
-//TARGET INFO GETTERS
-public static Pose3d targetPose3dCamSpace = LimelightLib.getTargetPose3d_CameraSpace(limelight);
-public static double targetID = LimelightLib.getFiducialID(limelight);
-public static double[] targetColor = LimelightLib.getTargetColor(limelight);
+    public Vision() {
+        updateValues();
+    }
 
-// Extract the position of the target from the Pose3d object
-public static Translation3d targetPosition = targetPose3dCamSpace.getTranslation();
+    private void updateValues() {
+        try {
+            // Coordinate values
+            tX = LimelightLib.getTX(LIMELIGHT);
+            tY = LimelightLib.getTY(LIMELIGHT);
+            tA = LimelightLib.getTA(LIMELIGHT);
+            tV = LimelightLib.getTV(LIMELIGHT);
 
+            // Robot pose values
+            botPose3d = LimelightLib.getBotPose3d(LIMELIGHT);
+            botPoseTargetSpace = LimelightLib.getBotPose_TargetSpace(LIMELIGHT);
+            botPose3dBlue = LimelightLib.getBotPose_wpiBlue(LIMELIGHT);
+            botPose3dRed = LimelightLib.getBotPose_wpiRed(LIMELIGHT);
+            botPose = LimelightLib.getBotPose(LIMELIGHT);
+            botPose2d = LimelightLib.getBotPose2d(LIMELIGHT);
+            botPose2dBlue = LimelightLib.getBotPose2d_wpiBlue(LIMELIGHT);
+            botPose2dRed = LimelightLib.getBotPose2d_wpiRed(LIMELIGHT);
 
-// Calculate the distance between the robot and the target using the position of the target
-double distance = Math.sqrt(Math.pow(targetPosition.getX(), 2) + Math.pow(targetPosition.getY(), 2) + Math.pow(targetPosition.getZ(), 2));
+            // Camera pose values
+            camPoseRobotSpace = LimelightLib.getCameraPose3d_RobotSpace(LIMELIGHT);
+            camPose3dRobotSpace = LimelightLib.getCameraPose3d_TargetSpace(LIMELIGHT);
+            camPose2dRobotSpace = LimelightLib.getCameraPose_TargetSpace(LIMELIGHT);
 
-//RANDOM OTHER STUFF GETTERS
-public static double pipelineIndex = LimelightLib.getCurrentPipelineIndex(limelight);
-public static double captureLatency = LimelightLib.getLatency_Capture(limelight);
-public static double pipelineLatency = LimelightLib.getLatency_Pipeline(limelight);
+            // Target info values
+            targetPose3dCamSpace = LimelightLib.getTargetPose3d_CameraSpace(LIMELIGHT);
+            targetID = LimelightLib.getFiducialID(LIMELIGHT);
+            targetColor = LimelightLib.getTargetColor(LIMELIGHT);
+            targetPosition = targetPose3dCamSpace.getTranslation();
 
+            // Latency values
+            pipelineIndex = LimelightLib.getCurrentPipelineIndex(LIMELIGHT);
+            captureLatency = LimelightLib.getLatency_Capture(LIMELIGHT);
+            pipelineLatency = LimelightLib.getLatency_Pipeline(LIMELIGHT);
 
-  /**
-   * CALCULATE THE DISTANCE TO THE NEAREST APRILTAG
-   *
-   * @param ty        THE CURRENT TY VALUE (USE A SUPPLIER)
-   * @param armAngle  THE CURRENT ARM ANGLE VALUE (USE A SUPPLIER)
-   * @param targetID  THE CURRENT TARGET ID (USE A SUPPLIER)
-   * @param type      THE TYPE OF APRILTAG ("speaker", "amp", "hp-station", "stage", "any")
-   */
-//FUNCTION FOR CALCULATING APRILTAG DISTANCE
-public static double calculateDistance(double ty, double armAngle, double targetID, String type) {
+        } catch (Exception e) {
+            LOGGER.log(Level.SEVERE, "Failed to update values from Limelight", e);
+        }
+    }
 
-  //CONSTANTS
-  double armLength = 12; //PLACEHOLDER VALUE
-  double pivotOffset = 12; //PLACEHOLDER VALUE
-  double limelightCenter = 12;
-  double goalHeightInches = 12;;
+    public void createVisionDashboard() {
+        SmartDashboard.putNumber("Latency", pipelineLatency);
+        SmartDashboard.putString("Pipeline", getPipelineName((int) pipelineIndex));
+    }
 
-  //CALCULATIONS
-  double angleInRadians = Math.toRadians(armAngle); //CONVERT THE ANGLE TO RADIANS
-  double verticalComponent = armLength * Math.cos(angleInRadians); //CALCULATE VERTICAL COMPONENT OF TRIANGLE
-  double limelightLensHeightInches = verticalComponent + pivotOffset + limelightCenter; //FIND THE HEIGHT OF THE LIMELIGHT LENS FROM THE FLOOR
+    private String getPipelineName(int index) {
+        switch (index) {
+            case 1: return "Neural Net";
+            case 2: return "Apriltags";
+            case 3: return "Color Filtering";
+            case 4: return "Driving";
+            default: return "Unknown";
+        }
+    }
 
-  double targetOffsetAngle_Vertical = ty;
-  double limelightMountAngleDegrees = armAngle + 12; //CHANGE THIS VALUE WHEN WE MEASURE LIMELIGHT ANGLE
-
-  double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
-  double angleToGoalRadians = Math.toRadians(angleToGoalDegrees);
-  double distanceInches = (goalHeightInches - limelightLensHeightInches) / Math.tan(angleToGoalRadians);
-
-  double distanceFeet = distanceInches/12;
-  // double distanceMeters = distanceFeet/3.281;
-  // double distanceYards = distanceFeet/3;
-
-  return distanceFeet;
-  //return distanceMeters;
-  //distanceYards;
-
-  
-}
-
-public static void createVisionDashboard() {
-  SmartDashboard.putNumber("Latency", pipelineLatency);
-
-  if (pipelineIndex == 1) {
-    SmartDashboard.putString("Pipeline:", "Neural Net");
-  } else if (pipelineIndex == 2) {
-    SmartDashboard.putString("Pipeline:", "Apriltags");
-  } else if (pipelineIndex ==  3) {
-    SmartDashboard.putString("Pipeline:", "Color Filtering");
-  } else if (pipelineIndex == 4) {
-    SmartDashboard.putString("Pipeline:", "Driving");
-  }
-
-  
-  
-}
-
-
-
-Vision() {
-  
-}
-
-  @Override
-  public void periodic() {
-    // This method will be called once per scheduler run
-  }
+    @Override
+    public void periodic() {
+        updateValues(); // Update values periodically
+    }
 }
