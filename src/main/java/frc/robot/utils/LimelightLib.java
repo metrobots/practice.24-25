6b0323c9acf15d8ba9a01c1eab5351cf661b25a2
Mateo Johnson <johnson.12@themetroschool.org>
2024-07-10 22:06:11 -0400

    changes

--- src/main/java/frc/robot/utils/LimelightLib.java
+++ src/main/java/frc/robot/utils/LimelightLib.java
@@ -637,6 +637,7 @@ public class LimelightLib {
     }
 
 
+    @SuppressWarnings("deprecation")
     public static URL getLimelightURLString(String tableName, String request) {
         String urlString = "http://" + sanitizeName(tableName) + ".local:5807/" + request;
         URL url;
