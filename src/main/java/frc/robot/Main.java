c5b923315af9fed5a2ea567ea86c466000ead4c9
metrobots <128004966+metrobots@users.noreply.github.com>
2024-09-17 10:03:41 -0400

    Update Main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -2,12 +2,9 @@
 //Open Source Software; you can modify and/or share it under the terms of
 //the WPILib BSD license file in the root directory of this project.
 
-
 package frc.robot;
 
-
 import edu.wpi.first.wpilibj.RobotBase;
-
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
